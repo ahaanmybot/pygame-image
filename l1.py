0d47107851efb076c6184d516976d07f5cc7e004
ahaanmybot <ahaanbinaj@gmail.com>
2026-03-28 11:15:24 +0400

    Refactor l1.py for improved code formatting
    Reformat code for better readability and structure.

--- l1.py
+++ l1.py
@@ -1,15 +1,21 @@
 import pygame
+
 pygame.init()
-screen = pygame.display.set_mode
-((400,500))
-bg_image = pygame.transform.scale(pygame.image.load('bg.jpg').convert(),(400,500))
+
+screen = pygame.display.set_mode((400, 500))
+
+bg_image = pygame.transform.scale(
+    pygame.image.load('bg.jpg').convert(), (400, 500)
+)
+
 done = False
+
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-           
-    screen.blit(bg_image,(0,0))
-    pygame.display.flip()
-pygame.quit()
 
+    screen.blit(bg_image, (0, 0))
+    pygame.display.flip()
+
+pygame.quit()
