8246b83ca02792196b21ce83f6aafc29c08c0fe7
ahaanmybot <ahaanbinaj@gmail.com>
2026-03-28 10:50:23 +0400

    Add files via upload

---
+++ l1.py
@@ -0,0 +1,15 @@
+import pygame
+pygame.init()
+screen = pygame.display.set_mode
+((400,500))
+bg_image = pygame.transform.scale(pygame.image.load('bg.jpg').convert(),(400,500))
+done = False
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+           
+    screen.blit(bg_image,(0,0))
+    pygame.display.flip()
+pygame.quit()
+
